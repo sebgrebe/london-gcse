ab6472cc6dbe2092fede20b091d011a03cb49685
sebgrebe <sebbecker@gmx.net>
2017-09-11 21:19:35 +0100

    add project

---
+++ app/svg.js
@@ -0,0 +1,116 @@
+$.ajax({
+	url: '/data',
+	error: (xhr,errorType) => {
+			console.log(errorType)
+		},
+	success: (data) => {
+		console.log(data);
+		const dataset = data;       
+		const padding_left = 70;
+		const padding_top = 20;
+		const padding_bottom = 40;
+		const padding_right = 120;
+		const w = 1000;
+		const h = window.innerHeight-200;
+		const graph = document.getElementById('graph');
+		function gather(data,type) {
+			var array = []
+			for (var i=0;i<dataset.length;i++) {
+				array.push(dataset[i][type])
+			}
+			return array
+		}
+		var income = gather(dataset,'income');
+		var gcse_perc = gather(dataset,'gcse');
+		const svg = d3.select(graph)
+		    .append('svg')
+		    .attr('width',w)
+		    .attr('height',h);
+		const xScale = d3.scaleLinear()
+			.domain([30000,d3.max(income)])
+			.range([padding_left,w-padding_right])
+		const yScale = d3.scaleLinear()
+			.domain([55,d3.max(gcse_perc)])
+			.range([h-padding_bottom,padding_top])
+		const xAxis = d3.axisBottom(xScale)
+        const yAxis = d3.axisLeft(yScale)
+
+		svg.selectAll('g')
+			.data(dataset)
+			.enter()
+			.append('circle')
+			.attr('cx',(d) => xScale(d['income']))
+			.attr('cy',(d) => yScale(d['gcse']))
+			.attr('fill','#39CCCC')
+			.attr('r',3)
+			.attr('class','circle')
+
+		//tooltips
+       	svg.selectAll('circle')
+      		.data(dataset)
+      		.on('mouseover',(d,i) =>
+             svg.append('foreignObject')
+             	.attr("x", xScale(d['income']))
+             	.attr("y", yScale(d['gcse']) + 10)
+             	.attr('width', 120)
+             	.attr('height',45)
+             	.append('xhtml:div')
+             	.html('<b>'+d['area']+'</b><br>Income: '+'£'+d['income']+'<br>GCSE: '+d['gcse']+'%')
+             	.attr('class','tip')
+             	)
+      		.on('mouseout',() =>
+            	 svg.selectAll('div.tip').remove()
+             	)
+
+		//x-Axis
+       svg.append('g')
+              .attr("transform", "translate(0, " + (h - padding_bottom) + ")")
+              .call(xAxis)
+                                   
+       //y-Axis
+       svg.append('g')
+              .attr("transform", "translate("+padding_left+", 0)")
+              .call(yAxis)
+
+       //y-label
+       svg.append('text')
+       .text("% of pupils achieving 5 A*-C GCSEs")
+       .attr('x',20)
+       .attr('y',70)
+       .attr('class','label_y')
+
+       //x-label
+       svg.append('text')
+       .text("Median income household (in £)")
+       .attr('x',330)
+       .attr('y',h)
+       .attr('class','label_x')
+		}
+	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require('express')
+var app = express()
+var XLSX = require('xlsx')
+
+const port = 3000;
+
+app.use(express.static('app'))
+app.get( '/', function( req, res ) {
+    res.sendFile('app/index.html' );
+  });
+app.get('/data',function(req,res) {
+	var workbook = XLSX.readFile('data.xlsx');
+	var sheet_with_areanames = workbook.Sheets['iadatasheet1'];
+	var sheet_with_gcses = workbook.Sheets['iadatasheet5'];
+	var sheet_with_income = workbook.Sheets['iadatasheet4'];
+	var area_names = [];
+	var gcse_perc = [];
+	var income = [];
+	for (var i=4;i<40;i++) {
+		area_names.push(sheet_with_areanames['B'+i]['v'])
+	}
+	for (var i=4;i<40;i++) {
+		gcse_perc.push(sheet_with_gcses['I'+i]['v']);
+	}
+	for (var i=4;i<40;i++) {
+		income.push(sheet_with_income['DX'+i]['v']);
+	}
+	var dataset = [];
+	for (var i=0;i<income.length;i++) {
+		var obj = new Object();
+		obj['area'] = area_names[i];
+		obj['gcse'] = gcse_perc[i];
+		obj['income'] = income[i];
+		var j=0;
+		while (obj[Object.keys(obj)[j]] != 'n/a' && j < 3) {
+			j++
+		}
+		if (j===3) {dataset.push(obj)}
+	}
+	res.send(dataset)
+})
+app.listen(port);
+
+// app.listen(port, (err) => {  
+//   if (err) {
+//     return console.log('something bad happened', err)
+//   }
+
+//   console.log(`server is listening on ${port}`)
+// })  
